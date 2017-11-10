e49563e038582b41397e37827cc410698fee8127
Chris Gullickson <cgullickson5@gmail.com>
2017-11-10 22:55:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,11 +28,10 @@ def won?(board)
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combo
     else
-      false
-      
+        false
     end
-    
-    
+
+
 
   end
 
